1e1d7a09b8522575122ce7a4a5bc55c56c700ea5
leonardo <leonard-sk8@hotmail.com>
2020-11-30 16:50:30 -0500

    Ajustes finales

--- main.py
+++ main.py
@@ -8,34 +8,37 @@
 import math
 
 
-def calcularDivisionModular(a, b, m):
-    a = a % m
-    inv = moduloInverso(b, m)
+def calcularDivisionModular(a, b, Zn):
+    a = a % Zn
+    inv = moduloInverso(b, Zn)
     if (inv == -1):
         print("La división no esta definida,")
     else:
-        print("El resultado de la división es: ", (inv * a) % m)
+        print("El resultado de la división es: ", (inv * a) % Zn)
 
 
-def moduloInverso(b, m):
-    g = math.gcd(b, m)
+def moduloInverso(b, Zn):
+    g = math.gcd(b, Zn)
     if (g != 1):
         return -1
     else:
-        # If b and m are relatively prime,
-        # then modulo inverse is b^(m-2) mode m
-        return pow(b, m - 2, m)
+        # Si b y m son primos relativos,
+        # El modulo inverso es b^(m-2) modulo Zn
+        return pow(b, Zn - 2, Zn) # Lo mismo que b^(m-2) % Zn
 
 
 if __name__ == '__main__':
     print('CRIPTOLOGIA')
-    Zn = int(input('INGRESE EL Zn: '))
-    a = int(input('INGRESE EL A: '))
-    b = int(input('INGRESE EL B: '))
+    Zn = int(input('INGRESE EL Zn: '
+                   ''))
+    a = int(input('INGRESE EL A: '
+                  ''))
+    b = int(input('INGRESE EL B: '
+                  ''))
     suma = (a + b) % Zn
     resta = (a - b) % Zn
     Multiplicacion = (a * b) % Zn
-    division = (a / b) % Zn
+    print('')
     print('Suma: ', suma)
     print("Resta: ", resta)
     print("Multiplicación: ", Multiplicacion)
